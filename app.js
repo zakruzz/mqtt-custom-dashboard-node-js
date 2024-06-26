33d11170f58dbdf3ea3a18532f6f986278945bde
zakruzz <zakruz23.1@gmail.com>
2024-06-26 16:59:43 +0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const port = 3001;
+import http from 'http';
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -35,6 +36,19 @@ app.use((req, res, next) => {
 // Routes
 const dashboardRouter = require('./routes/dashboard');
 app.use('/', dashboardRouter); // Use the router for the root path
+
+// Create a server object
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Write some text to the response
+  res.end('Welcome to my simple Node.js app!');
+});
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
